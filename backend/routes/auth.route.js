ecfff7f29d7b51a2a695a18e7994c3aafb466faa
Abhishek Rathore <144262140+Abhu0407@users.noreply.github.com>
2025-07-20 00:29:09 +0530

    Add files via upload

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { login, signup, getUserInfo } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import  unload  from '../middleware/upload.middleware.js';
 
 const router = express.Router();
 
@@ -9,17 +8,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.get("/getUser", protectRoute, getUserInfo);
 
-router.post("/upload-image", unload.single("image"), (req, res) => {
-    // Handle image upload
-    if(!req.file){
-        return res.status(400).json({ message: "No file uploaded" });
-    }
 
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-        req.file.filename
-    }`;
-
-    res.status(200).json({ imageUrl});
-});
 
 export default router;
---
+++ backend/routes/income.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { addIncome, getAllIncome, deleteIncome, downloadIncomeExcel } from "../controllers/income.controller.js";
+
+import { protectRoute } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+router.post("/add", protectRoute, addIncome);
+router.get("/all", protectRoute, getAllIncome);
+router.delete("/:id", protectRoute, deleteIncome);
+router.get("/downloadexcel", protectRoute, downloadIncomeExcel);
+
+export default router;
