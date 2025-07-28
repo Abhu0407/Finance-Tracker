d4ca284545e3a76d26f576b5bd1d5ad34827bcc8
Abhishek Rathore <144262140+Abhu0407@users.noreply.github.com>
2025-07-28 15:34:40 +0530

    Add files via upload

--- frontend/finance-tracker/src/utils/apiPaths.js
+++ frontend/finance-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5001";
+export const BASE_URL = import.meta.env.MODE ==="development" ? "http://localhost:5001" : "/";
 
 //utils/apiPath.js
 export const API_PATH = {
