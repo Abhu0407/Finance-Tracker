43082ebf8be8a6c5baab9b82ced9c7af324bceb1
Abhishek Rathore <144262140+Abhu0407@users.noreply.github.com>
2025-07-30 16:24:34 +0530

    Add files via upload

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -92,7 +92,7 @@ export const getUserInfo = async (req, res) => {
     try {
         const user = await User.findById(req.user._id).select("-password");
 
-        if(!user){
+        if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
 
--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,8 @@ app.use("/api/expense", expenseRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/finance-tracker/dist")));
-    
     app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/finance-tracker", "dist", "index.html"));
     });
